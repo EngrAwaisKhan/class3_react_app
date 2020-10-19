f751b4ead42ae84d5d201b4dedc40733d967f8d1
Muhammad Awais Khan <66188894+EngrAwaisKhan@users.noreply.github.com>
2020-10-19 12:02:16 +0500

    change menu items

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import Dinner from './dinner.js';
 function App() {
   return (
     <div>
-      <Dinner dishname ="chicken shorma" sweetname="keer"/>
+      <Dinner dishname ="chicken baryani" sweetname="keer"/>
       <hr/>
-      <Dinner dishname ="chicken takka" sweetname ="custer"/>
+      <Dinner dishname ="mutton" sweetname ="custer"/>
       <hr/>
       <Dinner dishname ="fish" sweetname ="sala rice"/>
     </div>
